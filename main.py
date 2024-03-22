89c5bbba93760bf6202a4f9a129b7fff340e127e
Sheldon <lhanco@gmail.com>
2024-03-22 11:27:39 +0700

    Day 31: Add URL endpoint for yearly and all dates one station.

--- main.py
+++ main.py
@@ -23,43 +23,29 @@ def about(station, date):
             "temperature": temperature}
 
 
+@app.route("/api/v1/<station>")
+def all_dates_one_station(station):
+    filepath = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filepath, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient="records")
+    return result
+
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly_one_station(station, year):
+    filepath = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+
+    # This way will keep the DATE column type as DateTime type in the output
+    df = pd.read_csv(filepath, skiprows=20, parse_dates=["    DATE"])
+    result = df[df["    DATE"].dt.year == int(year)].to_dict(orient="records")
+
+    # This way will keep the DATE column type as String type in the output
+    # df = pd.read_csv(filepath, skiprows=20)
+    # df["    DATE"] = df["    DATE"].astype(str)
+    # result = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+
+    return result
+
+
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
